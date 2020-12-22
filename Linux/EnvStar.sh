70be2b6c8b364b31bb97cfcbf668248963aa2e62
Itai Danielli <Danielli.Itai@gmail.com>
2020-12-22 09:52:53 +0200

    Move environment start to Linux folder.

---
+++ Linux/EnvStar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Keep linux End of line conversion.
+echo "Baseic environment."
+
+
+
+
+#Environment Variables.
+export HOME_DIR=~
+export WORK_DIR=~
+export PROJ_DIR=$HOME_DIR/WebSrvCoderApprentice
+
+
+
+
+
+source	EnvSetup.sh
+
+
+
+
